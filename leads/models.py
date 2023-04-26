63c52201830983d38af5b526e0b473bbb9bf84e7
Julind Pullumbi <julind.pullumbi@gmail.com>
2023-04-26 22:45:08 +0200

    Leads App & User Model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    pass
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
